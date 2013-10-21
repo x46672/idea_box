f48f0e93e7d7fcb4d3af0f391c1fae556015b190
Kevin Powell <kevin.m.powell04@gmail.com>
2013-10-20 22:04:14 -0600

    first commit on the idea box

---
+++ app.rb
@@ -0,0 +1,45 @@
+require './idea'
+
+class IdeaBoxApp < Sinatra::Base
+  set :method_override, true
+
+  not_found do
+    erb :error
+  end
+
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  delete '/:id' do |id|
+    Idea.delete(id.to_i)
+    redirect '/'
+  end
+
+  get '/' do
+    erb :index, locals: {ideas: Idea.all}  
+  end
+
+  post '/' do
+    #idea = Idea.new(title: params['title'], description: params['idea_description'])
+    idea = Idea.new(params[:idea])
+    idea.save
+    redirect '/'
+  end
+
+  get '/:id/edit' do |id|
+    idea = Idea.find(id.to_i)
+    erb :edit, locals: {id: id, idea: idea}
+  end
+
+  put '/:id' do |id|
+    data = {
+      :title => params['idea_title'],
+      :description => params['idea_description']
+    }
+    Idea.update(id.to_i, data)
+    redirect '/'
+  end
+
+
+end
